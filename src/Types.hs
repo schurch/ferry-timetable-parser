c16a1ef53379764c2ce9ce6a22d08c2f715ace9c
Stefan Church <stefan.church@gmail.com>
2017-05-13 15:44:49 +1200

    Parse everything except VehicleJourneys

---
+++ src/Types.hs
@@ -0,0 +1,77 @@
+module Types where
+
+import Data.Time
+
+data AnnotatedStopPointRef = AnnotatedStopPointRef
+  { stopPointRef :: String
+  , commonName :: String
+  } deriving (Show)
+
+data RouteSection = RouteSection
+  { routeSectionId :: String
+  , routeLink :: RouteLink
+  } deriving (Show)
+
+data RouteLink = RouteLink
+  { routeLinkId :: String
+  , fromStopPointRef :: String
+  , toStopPointRef :: String
+  , routeDirection :: String
+  } deriving (Show)
+
+data Route = Route
+  { routeId :: String
+  , routeDescription :: String
+  , routeSectionRef :: String
+  } deriving (Show)
+
+data JourneyPatternSection = JourneyPatternSection
+  { journeyPatterSectionId :: String
+  , journeyPatternTimingLinks :: [JourneyPatternTimingLink]
+  } deriving (Show)
+
+data JourneyPatternTimingLink = JourneyPatternTimingLink
+  { journeyPatternTimingLinkId :: String
+  , journeyPatternFromStopPointRef :: String
+  , journeyPatternFromTimingStatus :: String
+  , journeyPatternToStopPointsRef :: String
+  , journeyPatternToTimingStatus :: String
+  , routeLinkRef :: String
+  , journeyDirection :: String
+  , runTime :: String
+  } deriving (Show)
+
+data Operator = Operator
+  { operatorId :: String
+  , nationalOperatorCode :: String
+  , operatorCode :: String
+  , operatorShortName :: String
+  } deriving (Show)
+
+data Service = Service
+  { serviceCode :: String
+  , lines :: [Line]
+  , startDate :: Maybe UTCTime
+  , endDate :: Maybe UTCTime
+  , registeredOperatorRef :: String
+  , mode :: String
+  , description :: String
+  , standardService :: StandardService
+  } deriving (Show)
+
+data Line = Line
+  { lineId :: String
+  , lineName :: String
+  } deriving (Show)
+
+data StandardService = StandardService
+  { origin :: String
+  , destination :: String
+  , journeyPatterns :: [JourneyPattern]
+  } deriving (Show)
+
+data JourneyPattern = JourneyPattern
+  { journeyPatternId :: String
+  , journeyPatternDirection :: String
+  , journeyPatternSectionRef :: String
+  } deriving (Show)
